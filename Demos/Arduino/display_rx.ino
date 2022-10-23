bc7b50823755a89074cbde0fe533ef558e6071e6
askiiart <benzimm07@gmail.com>
2022-10-23 12:55:28 -0500

    Fix wrong whitespace

--- Demos/Arduino/display_rx.ino
+++ Demos/Arduino/display_rx.ino
@@ -54,8 +54,8 @@ void loop() {
     }
     myTransfer.sendData(myTransfer.bytesRead);
 
-		int x = int(myTransfer.packet.rxBuff);
-		if((x % 2) > 0) { digitalWrite(pin1, HIGH); } else { digitalWrite(pin1, LOW); }
+	int x = int(myTransfer.packet.rxBuff);
+    if((x % 2) > 0) { digitalWrite(pin1, HIGH); } else { digitalWrite(pin1, LOW); }
     if((x % 4) > 1) { digitalWrite(pin2, HIGH); } else { digitalWrite(pin2, LOW); }
     if((x % 8) > 3) { digitalWrite(pin3, HIGH); } else { digitalWrite(pin3, LOW); }
     if((x % 16) > 7) { digitalWrite(pin4, HIGH); } else { digitalWrite(pin4, LOW); }
